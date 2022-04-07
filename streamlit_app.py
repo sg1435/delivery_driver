d268dc68d33ee6f2119fb79b8aa3af48e76b4054
serhat <sguven1@yandex.com>
2022-04-07 15:09:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,9 @@ pick_and_drop_time = st.slider('PICK AND DROP TIME (min.)', 1, 60)
 desired_hourly_earning = st.text_input('DESIRED HOURLY EARNING', value="")
 desired_hourly_earning = int(desired_hourly_earning)
 
-
-mil = st.slider('DELIVERY MILE',0,100, 0.5)
+mil = st.slider('DELIVERY MILE',0.0,50.0,0.5)
 
 price_max = mil/mpg*oil_price + mil/mph*desired_hourly_earning + pick_and_drop_time/60*desired_hourly_earning
 price_min = mil/mpg*oil_price + mil/mph*desired_hourly_earning*2 + pick_and_drop_time/60*desired_hourly_earning
-
-st.text(price)
+st.button('CALCULATE THE OFFER PRICES')
+st.text('minimum price is ' + str(price_min))
