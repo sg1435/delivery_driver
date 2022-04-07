9fef99878d7030f7a7ffa0009426baeced76287e
serhat <sguven1@yandex.com>
2022-04-07 15:30:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,8 @@ import streamlit as st
 
 
 
-oil_price = st.text_input('GAS PRICE', value = '4.17')
-oil_price = int(oil_price)
-mpg = st.text_input('MPG', value = '25')
-mpg = int(mpg)
+oil_price = float(st.text_input('GAS PRICE', value = '4.17'))
+mpg = int(st.text_input('MPG', value = '25'))
 mph = int(st.text_input('ESTIMATED MPH', value = '30'))
 pick_and_drop_time = int(st.text_input('PICK AND DROP TIME (min.)', '10'))
 desired_hourly_earning = int(st.text_input('DESIRED HOURLY EARNING', value="20"))
