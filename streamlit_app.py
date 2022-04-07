135f64d7d849d6bfd49dc88c8cff329006f52a7e
serhat <sguven1@yandex.com>
2022-04-07 15:24:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
+
+'''
 oil_price = st.slider('GAS PRICE', 1.0, 10.0, 0.1)
 mpg = st.slider('MPG', 1, 100)
 mph = st.slider('ESTIMATED MPH', 10, 100)
 pick_and_drop_time = st.slider('PICK AND DROP TIME (min.)', 1, 60)
-desired_hourly_earning = st.text_input('DESIRED HOURLY EARNING', value="")
-desired_hourly_earning = int(desired_hourly_earning)
+'''
 
+oil_price = int(st.text_input('GAS PRICE', value = '4.17'))
+mpg = int(st.text_input('MPG', value = '25'))
+mph = int(st.text_input('ESTIMATED MPH', value = '30'))
+pick_and_drop_time = int(st.text_input('PICK AND DROP TIME (min.)', '10'))
+desired_hourly_earning = int(st.text_input('DESIRED HOURLY EARNING', value="20"))
 mil = st.slider('DELIVERY MILE',0.0,50.0,1.0)
 
 price_min = mil/mpg*oil_price + mil/mph*desired_hourly_earning + pick_and_drop_time/60*desired_hourly_earning
